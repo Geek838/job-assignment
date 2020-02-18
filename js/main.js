231cf3ca382e086b9141d930131f3d5723c54aba
Geek838 <geek838@gmail.com>
2020-02-18 18:08:28 +0400

    update version without bootstrap

--- js/main.js
+++ js/main.js
@@ -18,3 +18,47 @@ ScrollReveal().reveal('.tagline', {
 ScrollReveal().reveal('.punchline', {
     delay: 1000
 });
+
+//navbar dropdowns and burger menu 
+
+const dropdown = document.querySelector('.dropdown');
+const subMenu = document.querySelector('.sub-menu')
+const subMenuChildren = document.querySelectorAll('.sub-menu li')
+
+dropdown.addEventListener('click', (e) => {
+    e.preventDefault();
+    subMenu.classList.toggle('active');
+})
+
+const navSlider = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        if (nav.style.opacity) {
+            nav.style.opacity = '';
+        } else {
+            nav.style.opacity = '1';
+        }
+        navLinks.forEach((element, index) => {
+            if (element.style.animation) {
+                element.style.animation = ''
+            } else {
+                element.style.animation = ''
+                element.style.animation = `navLinksFade 0.5s ease-in forwards ${index/7 + 0.2}s`
+                subMenuChildren.forEach((e) => {
+                    e.style.animation = '';
+                    e.style.opacity = '1';
+                    e.style.transformX = '0px';
+
+                })
+            }
+        });
+        burger.classList.toggle('toggle');
+    })
+
+
+}
+
+navSlider();
